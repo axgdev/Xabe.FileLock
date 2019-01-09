99d892632228d854c4f637e06b108213c82e3782
Axel Garcia <axgdev@outlook.com>
2019-01-09 13:14:20 +0100

    Add TimeoutLockModel which has CachedReleaseDate

---
+++ Xabe.FileLock/TimeoutLockModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xabe
+{
+    internal class TimeoutLockModel: LockModel
+    {
+        public TimeoutLockModel(string path) : base(path)
+        {
+        }
+
+        internal DateTime CachedReleaseDate;
+
+        internal new async Task<bool> TrySetReleaseDate(DateTime date)
+        {
+            var releaseDateIsSet = await base.TrySetReleaseDate(date);
+            if (releaseDateIsSet)
+            {
+                CachedReleaseDate = date;
+            }
+
+            return releaseDateIsSet;
+        }
+    }
+}
