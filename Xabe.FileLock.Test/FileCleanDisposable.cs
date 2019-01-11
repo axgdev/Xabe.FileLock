a46fcbcfb189b0aa37014bff063854575f335254
Axel Garcia <axgdev@outlook.com>
2019-01-11 17:57:46 +0100

    Add alternative Path for easier clean up

---
+++ Xabe.FileLock.Test/FileCleanDisposable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using Path = Xabe.Test.FileLockTestPath;
+
+namespace Xabe.Test
+{
+    public class FileCleanDisposable: IDisposable
+    {
+        public void Dispose()
+        {
+            if (Directory.Exists(Path.TempFolderPath))
+            {
+                Directory.Delete(Path.TempFolderPath, true);
+            }
+            Assert.False(Directory.Exists(Path.TempFolderPath));
+        }
+    }
+}
---
+++ Xabe.FileLock.Test/FileLockTestPath.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Xabe.Test
+{
+    internal class FileLockTestPath
+    {
+        public static readonly string TempFolderPath = Path.Combine(Path.GetTempPath(), "FileLockTests");
+
+        public static string GetTempFileName()
+        {
+            if (!Directory.Exists(TempFolderPath))
+            {
+                Directory.CreateDirectory(TempFolderPath);
+            }
+            var fileInfo = new FileInfo(Path.Combine(TempFolderPath, Path.GetRandomFileName()));
+            fileInfo.Create();
+            return fileInfo.Name;
+        }
+
+        public static string ChangeExtension(string path, string extension)
+        {
+            return Path.ChangeExtension(path, extension);
+        }
+    }
+}
